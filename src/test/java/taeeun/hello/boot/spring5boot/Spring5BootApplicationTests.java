f067b7aa4dd0e604be425d5880182e35741cc94b
TaeEun Park <ubub0322@gmail.com>
2023-07-04 11:07:06 +0900

    프로젝트 초기화

---
+++ src/test/java/taeeun/hello/boot/spring5boot/Spring5BootApplicationTests.java
@@ -0,0 +1,13 @@
+package taeeun.hello.boot.spring5boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
