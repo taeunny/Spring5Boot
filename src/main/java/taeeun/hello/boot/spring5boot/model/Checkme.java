1e22b9a9f14cb0dd9402022acf8da60f67106103
TaeEun Park <ubub0322@gmail.com>
2023-07-06 15:50:36 +0900

    회원가입 : 실명 확인 정보용 클래스

---
+++ src/main/java/taeeun/hello/boot/spring5boot/model/Checkme.java
@@ -0,0 +1,16 @@
+package taeeun.hello.boot.spring5boot.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+
+public class Checkme {
+    private String name;
+    private String jumin1;
+    private String jumin2;
+}
