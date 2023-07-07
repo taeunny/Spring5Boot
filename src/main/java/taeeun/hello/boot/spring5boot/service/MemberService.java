3e6220f2d44b251e58f07ce7077ad9919acb765e
TaeEun Park <ubub0322@gmail.com>
2023-07-07 16:20:55 +0900

    우편번호 조회 : MemberService findZip 구현

--- src/main/java/taeeun/hello/boot/spring5boot/service/MemberService.java
+++ src/main/java/taeeun/hello/boot/spring5boot/service/MemberService.java
@@ -1,5 +1,6 @@
 package taeeun.hello.boot.spring5boot.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import taeeun.hello.boot.spring5boot.model.Member;
 
 import java.util.List;
@@ -9,4 +10,6 @@ public interface MemberService {
     boolean saveMember(Member m);
 
     List<Member> readMember();
+
+    String findzip(String dong) throws JsonProcessingException;
 }
