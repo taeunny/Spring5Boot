b7859956b96c1e3622bf3205fca94d7d028c61e7
TaeEun Park <ubub0322@gmail.com>
2023-07-07 15:05:39 +0900

    회원가입 : JoinController joinmeok 추가

--- src/main/java/taeeun/hello/boot/spring5boot/controller/JoinController.java
+++ src/main/java/taeeun/hello/boot/spring5boot/controller/JoinController.java
@@ -3,11 +3,14 @@ package taeeun.hello.boot.spring5boot.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import taeeun.hello.boot.spring5boot.model.Checkme;
+import taeeun.hello.boot.spring5boot.model.Member;
+import taeeun.hello.boot.spring5boot.service.MemberService;
 
 import javax.servlet.http.HttpSession;
 
@@ -15,6 +18,8 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/join")
 public class JoinController {
+
+    @Autowired MemberService msrv;
 
     Logger logger = LogManager.getLogger(JoinController.class);
     @GetMapping("/agree")
@@ -53,6 +58,20 @@ public class JoinController {
         return "join/joinme";
 
     }
+
+    @PostMapping("/joinme")
+    public String joinmeok(Member m) {
+        logger.info("join/joinmeok 호출!!");
+
+        String viewPage = "redirect:/join/joinfail";
+
+        if (msrv.saveMember(m))
+            viewPage = "redirect:/join/joinok";
+
+        return viewPage;
+
+    }
+
     @GetMapping("/joinok")
     public String joinok() {
         logger.info("join/joinok 호출!!");
