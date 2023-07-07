0b4ba2c682978f5c3469d2ba14f4b77b5445e9a3
TaeEun Park <ubub0322@gmail.com>
2023-07-07 16:22:01 +0900

    우편번호 조회 : Zipcode 클래스 추가

---
+++ src/main/java/taeeun/hello/boot/spring5boot/model/Zipcode.java
@@ -0,0 +1,18 @@
+package taeeun.hello.boot.spring5boot.model;
+
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Zipcode {
+    private String zipcode;
+    private String sido;
+    private String gugun;
+    private String dong;
+    private String ri;
+    private String bunji;
+}
