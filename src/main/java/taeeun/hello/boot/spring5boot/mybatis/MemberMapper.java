5aa03c7d903bbb1327f7324f91575357c0c956af
TaeEun Park <ubub0322@gmail.com>
2023-07-07 16:23:03 +0900

    우편번호 조회 : mybatis findZipcode 맵핑 추가

--- src/main/java/taeeun/hello/boot/spring5boot/mybatis/MemberMapper.java
+++ src/main/java/taeeun/hello/boot/spring5boot/mybatis/MemberMapper.java
@@ -2,6 +2,7 @@ package taeeun.hello.boot.spring5boot.mybatis;
 
 import org.apache.ibatis.annotations.Mapper;
 import taeeun.hello.boot.spring5boot.model.Member;
+import taeeun.hello.boot.spring5boot.model.Zipcode;
 
 import java.util.List;
 @Mapper
@@ -9,5 +10,6 @@ public interface MemberMapper {
 
     int insertMember(Member m);
     List<Member> selectMember();
+    List<Zipcode> findZipcode(String dong);
 
 }
