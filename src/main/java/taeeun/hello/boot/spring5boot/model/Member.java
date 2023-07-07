7504ac52fbce4d77536ebc6f2c50c232e387eaa1
TaeEun Park <ubub0322@gmail.com>
2023-07-07 10:38:35 +0900

    회원가입 : Member 클래스 추가

---
+++ src/main/java/taeeun/hello/boot/spring5boot/model/Member.java
@@ -0,0 +1,24 @@
+package taeeun.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Member {
+    private String mno;
+    private String userid;
+    private String passwd;
+    private String name;
+    private String email;
+    private String zipcode;
+    private String addr1;
+    private String addr2;
+    private String phone;
+    private String regdate;
+
+
+}
