79d7bf941a5b70b080d4febc5a888aac3e08bf49
TaeEun Park <ubub0322@gmail.com>
2023-07-12 10:16:51 +0900

    게시판 : boardService 단위테스트

---
+++ src/test/java/taeeun/hello/boot/spring5boot/board/BoardServiceUnitTest.java
@@ -0,0 +1,36 @@
+package taeeun.hello.boot.spring5boot.board;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import taeeun.hello.boot.spring5boot.dao.BoardDAOImpl;
+import taeeun.hello.boot.spring5boot.model.Board;
+import taeeun.hello.boot.spring5boot.service.BoardService;
+import taeeun.hello.boot.spring5boot.service.BoardServiceImpl;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import({BoardServiceImpl.class, BoardDAOImpl.class})
+public class BoardServiceUnitTest {
+
+    // 객체 받아오기
+    @Autowired private BoardService bsrv;
+
+    @Test
+    @DisplayName("BoardService read Test")
+    void readBoard() {
+        List<Board> results = bsrv.readBoard();
+
+        // System.out.println(results);
+        assertNotNull(results);
+    }
+
+}
