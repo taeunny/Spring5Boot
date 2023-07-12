e1a56977867f372b0d88a90975ab9524779d835f
TaeEun Park <ubub0322@gmail.com>
2023-07-12 11:46:56 +0900

    게시판 : BoardController 페이징 처리 단위테스트

--- src/test/java/taeeun/hello/boot/spring5boot/board/BoardControllerUnitTest.java
+++ src/test/java/taeeun/hello/boot/spring5boot/board/BoardControllerUnitTest.java
@@ -25,9 +25,10 @@ public class BoardControllerUnitTest {
     @DisplayName("BoardController read Test")
     void readBoard() throws Exception {
 
-        mvc.perform(get("/board/list"))
-                    .andExpect(status().isOk())
-                    .andDo(print());
+        mvc.perform(get("/board/list")
+                        .param("cpg","1"))
+                        .andExpect(status().isOk())
+                        .andDo(print());
     }
 
 }
