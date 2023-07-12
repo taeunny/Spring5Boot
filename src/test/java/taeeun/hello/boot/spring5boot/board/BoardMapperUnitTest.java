75bb6b51d7d6c3d73943a07e338b16454c19d28d
TaeEun Park <ubub0322@gmail.com>
2023-07-12 18:04:10 +0900

    게시판 : write 단위테스트

--- src/test/java/taeeun/hello/boot/spring5boot/board/BoardMapperUnitTest.java
+++ src/test/java/taeeun/hello/boot/spring5boot/board/BoardMapperUnitTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.transaction.annotation.Transactional;
 import taeeun.hello.boot.spring5boot.model.Board;
 import taeeun.hello.boot.spring5boot.model.Member;
 import taeeun.hello.boot.spring5boot.mybatis.BoardMapper;
@@ -39,6 +40,20 @@ public class BoardMapperUnitTest {
         Board result = boardMapper.selectOneBoard(bno);
 
         assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("BoardMapper insert Test")
+    @Transactional
+    void insertBoard() {
+        Board b = new Board();
+        b.setUserid("abc123");
+        b.setTitle("테스트");
+        b.setContents("테스트");
+        b.setIpaddr("127.0.0.1");
+
+        int result = boardMapper.insertBoard(b);
+        assertEquals(result,1);
     }
 
 }
