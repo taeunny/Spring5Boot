0361d3f56b98a64ba59a56153ff81096c7c61b77
TaeEun Park <ubub0322@gmail.com>
2023-07-12 10:14:02 +0900

    게시판 : board mapper 단위테스트

---
+++ src/test/java/taeeun/hello/boot/spring5boot/board/BoardMapperUnitTest.java
@@ -0,0 +1,48 @@
+package taeeun.hello.boot.spring5boot.board;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import taeeun.hello.boot.spring5boot.model.Board;
+import taeeun.hello.boot.spring5boot.model.Member;
+import taeeun.hello.boot.spring5boot.mybatis.BoardMapper;
+import taeeun.hello.boot.spring5boot.mybatis.MemberMapper;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BoardMapperUnitTest {
+
+    @Autowired
+    private BoardMapper boardMapper;
+
+    @Test
+    @DisplayName("BoardMapper select Test")
+    void selectBoard() {
+        List<Board> results = boardMapper.selectBoard();
+
+        System.out.println(results);
+        assertNotNull(results);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
