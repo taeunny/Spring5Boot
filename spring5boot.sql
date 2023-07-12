d173d2471a0cea9f1705006201d8ed4fd94e4b24
TaeEun Park <ubub0322@gmail.com>
2023-07-12 10:08:46 +0900

    게시판 : board 테이블 생성

--- spring5boot.sql
+++ spring5boot.sql
@@ -19,3 +19,41 @@ values ('abc123','987xyz','tt','tt@abc123.co.kr',
         '123456','서울 관악구','블라블라','123-4567-8912');
 
 select  * from member2;
+
+
+-- board
+create table board2 (
+    bno         int      auto_increment,
+    title       varchar(100)      not null,
+    userid      varchar(18)       not null,
+    regdate     datetime          default current_timestamp,
+    thumbs      int               default 0,
+    views       int               default 0,
+    contents    text              not null,
+    ipaddr      varchar(15)       not null,
+    primary key (bno)
+    -- foreign key (userid) references member2(userid)
+);
+
+alter table board2
+    add constraint fkuid
+        foreign key (userid) references member2 (userid);
+
+
+insert into board2
+    (userid, title,contents, ipaddr)
+values ('abc123', ' 구글, 사람처럼 진찰하는','뇨뇨뇽','127.0.0.1');
+
+insert into board2
+(userid, title,contents, ipaddr)
+values ('abc123a', '파리 생제르망','뇨뇨뇽','127.0.0.1');
+
+insert into board2
+(userid, title,contents, ipaddr)
+values ('ubub0322' , '데이터센터 구축','뇨뇨뇽','127.0.0.1');
+
+
+
+select
+    count(userid) cnt, ceil(count(userid) /25) pages
+from board2;
