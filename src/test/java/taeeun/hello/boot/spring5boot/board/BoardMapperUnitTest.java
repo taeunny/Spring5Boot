79d420ef0bb77ed179ab85917dcde61aa4ec5ee2
TaeEun Park <ubub0322@gmail.com>
2023-07-12 11:45:29 +0900

    게시판 : boardMapper 페이징 처리 단위테스트

--- src/test/java/taeeun/hello/boot/spring5boot/board/BoardMapperUnitTest.java
+++ src/test/java/taeeun/hello/boot/spring5boot/board/BoardMapperUnitTest.java
@@ -15,7 +15,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BoardMapperUnitTest {
