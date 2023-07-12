25fa7569c9914cc465aeaac67769887766c51f0a
TaeEun Park <ubub0322@gmail.com>
2023-07-12 11:45:01 +0900

    게시판 : boardDAO 페이징 처리 단위테스트

--- src/test/java/taeeun/hello/boot/spring5boot/board/BoardDAOUnitTest.java
+++ src/test/java/taeeun/hello/boot/spring5boot/board/BoardDAOUnitTest.java
@@ -25,7 +25,10 @@ public class BoardDAOUnitTest {
     @Test
     @DisplayName("BoardDAO select Test")
     void selectBoard() {
-      List<Board> results = bdao.selectBoard();
+      int cpg = 1;
+      int stnum = (cpg-1) * 25;
+
+      List<Board> results = bdao.selectBoard(stnum);
 
       // System.out.println(results);
       assertNotNull(results);
