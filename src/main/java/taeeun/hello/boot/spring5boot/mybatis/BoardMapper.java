a274c84ac165dc2dfc6903092547de0a5c7eb14c
TaeEun Park <ubub0322@gmail.com>
2023-07-13 16:06:18 +0900

    게시판 검색 기능을 위한 코드 추가

--- src/main/java/taeeun/hello/boot/spring5boot/mybatis/BoardMapper.java
+++ src/main/java/taeeun/hello/boot/spring5boot/mybatis/BoardMapper.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 import taeeun.hello.boot.spring5boot.model.Board;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface BoardMapper {
@@ -17,5 +18,10 @@ public interface BoardMapper {
     List<Board> selectBoard(int stnum);
 
     int updateViewBoard(String bno);
+
+    int selectCountBoard();
+
+    List<Board> selectFindBoard(Map<String, Object> params);
+
 }
 
