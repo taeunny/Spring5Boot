75fab575b48918b110934d0a7fcf92e71c0fcfc2
TaeEun Park <ubub0322@gmail.com>
2023-07-14 16:02:48 +0900

    자료실 - pds, pdsattach 테이블 추가

--- spring5boot.sql
+++ spring5boot.sql
@@ -57,3 +57,53 @@ values ('ubub0322' , '데이터센터 구축','뇨뇨뇽','127.0.0.1');
 select
     count(userid) cnt, ceil(count(userid) /25) pages
 from board2;
+
+
+select * from board2
+where title like '%데이터%';
+
+
+-- pds
+create table pds (
+    pno          int               auto_increment,
+     title       varchar(100)      not null,
+     userid      varchar(18)       not null,
+     regdate     datetime          default current_timestamp,
+     thumbs      int               default 0,
+     views       int               default 0,
+     contents    text              not null,
+     ipaddr      varchar(15)       not null,
+     primary key (pno)
+);
+
+
+create table pdsattach  (
+    pano        int             auto_increment,
+    pno         int             not null,      -- 게시글 번호
+    fname       varchar(200)    not null,      -- uuid 포함 (다른 사람이 똑같은 파일 올리면 충돌, uuid:식별코드)
+    ftype       varchar(3)      not null,      -- 파일의 종류를 아이콘화(압축형식)
+    fsize       float           not null ,     -- 파일 용량
+    fdown       int             default 0,      -- 몇 번 다운받았는지
+    primary key (pano)
+);
+
+alter table pds
+    add constraint fkpuid
+    foreign key (userid) references member2 (userid);
+
+alter table pdsattach
+    add constraint fkpno
+        foreign key (pno) references pds (pno);
+
+
+
+
+
+
+
+
+
+
+
+
+
