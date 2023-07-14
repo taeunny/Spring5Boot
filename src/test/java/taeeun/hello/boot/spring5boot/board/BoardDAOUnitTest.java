1dc84f97ed79c2d879b984b517bba102c3dc562f
TaeEun Park <ubub0322@gmail.com>
2023-07-14 11:03:11 +0900

    게시판 검색 건수 BoardDAO 단위테스트

--- src/test/java/taeeun/hello/boot/spring5boot/board/BoardDAOUnitTest.java
+++ src/test/java/taeeun/hello/boot/spring5boot/board/BoardDAOUnitTest.java
@@ -82,4 +82,16 @@ public class BoardDAOUnitTest {
         assertNotNull(results);
     }
 
+    @Test
+    @DisplayName("BoardDAO countfindBoard Test")
+    void countfindBoard() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("findtype","titcont");
+        params.put("findkey","생제르망");
+
+       int result = bdao.countFindBoard(params);
+
+        assertNotNull(result);
+    }
+
 }
