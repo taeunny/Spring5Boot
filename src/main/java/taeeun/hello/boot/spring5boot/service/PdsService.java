a3856f3a6cc14c98ccbb33fdfa83ab26b6aa9e54
TaeEun Park <ubub0322@gmail.com>
2023-07-14 16:06:50 +0900

    자료실 - PdsService newPds 구현

---
+++ src/main/java/taeeun/hello/boot/spring5boot/service/PdsService.java
@@ -0,0 +1,12 @@
+package taeeun.hello.boot.spring5boot.service;
+
+import org.springframework.web.multipart.MultipartFile;
+import taeeun.hello.boot.spring5boot.model.Pds;
+
+import java.util.Map;
+
+public interface PdsService {
+
+   int newPds(Pds p);
+    boolean newPdsAttach(MultipartFile attach, int pinfo);
+}
---
+++ src/main/java/taeeun/hello/boot/spring5boot/service/PdsServiceImpl.java
@@ -0,0 +1,26 @@
+package taeeun.hello.boot.spring5boot.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import taeeun.hello.boot.spring5boot.dao.PdsDAO;
+import taeeun.hello.boot.spring5boot.model.Pds;
+
+import java.util.Map;
+
+@Service("psrv")
+@RequiredArgsConstructor
+public class PdsServiceImpl implements PdsService {
+
+    final PdsDAO pdao;
+    @Override
+    public int newPds(Pds p) {
+
+        return pdao.insertPds(p);
+    }
+
+    @Override
+    public boolean newPdsAttach(MultipartFile attach, int pno) {
+        return false;
+    }
+}
