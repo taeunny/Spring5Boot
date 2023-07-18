18457ce94003bea2da8ba73e5abc3332b6d1fcf9
TaeEun Park <ubub0322@gmail.com>
2023-07-18 13:35:01 +0900

    댓글 - PdsComment 클래스 추가

---
+++ src/main/java/taeeun/hello/boot/spring5boot/model/PdsComment.java
@@ -0,0 +1,19 @@
+package taeeun.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class PdsComment {
+    private String cno;
+    private String comments;
+    private String userid;
+    private String regdate;
+    private String ref;
+    private String pno;
+
+}
