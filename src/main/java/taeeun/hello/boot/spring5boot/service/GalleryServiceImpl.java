d5b8bf36d38c279ac95abd983aba5a2e424907a6
TaeEun Park <ubub0322@gmail.com>
2023-07-21 10:49:40 +0900

    갤러리: newGallery, newGalAttach 구현

---
+++ src/main/java/taeeun/hello/boot/spring5boot/service/GalleryServiceImpl.java
@@ -0,0 +1,51 @@
+package taeeun.hello.boot.spring5boot.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import taeeun.hello.boot.spring5boot.dao.GalleryDAO;
+import taeeun.hello.boot.spring5boot.model.GalAttach;
+import taeeun.hello.boot.spring5boot.model.Gallery;
+import taeeun.hello.boot.spring5boot.utils.GalleryUtils;
+
+import java.util.List;
+    @Service("gsrv")
+    @RequiredArgsConstructor
+    public class GalleryServiceImpl implements GalleryService {
+
+        final GalleryDAO gdao;
+        final GalleryUtils galUtils;
+
+        @Override
+        public List<Gallery> readGallery(Integer cpg) {
+
+            return gdao.selectGallery(cpg - 1);
+        }
+
+        @Override
+        public int countGallery() {
+            return 0;
+        }
+
+        @Override
+        public int newGallery(Gallery g) {
+            return gdao.insertGallery(g);
+        }
+
+        @Override
+        public boolean newGalAttach(List<MultipartFile> attachs, int gno) {
+            // 이미지 파일 저장후 파일 정보(들) 받아오기
+            GalAttach ga = galUtils.processUpload(attachs);
+
+            // 썸네일 이미지 생성
+            galUtils.makeThumbnail(ga);
+
+            // 이미지 파일 정보 저장
+            ga.setGno(gno +"");
+
+            return (gdao.insertGalAttach(ga) > 0) ? true : false;
+        }
+
+
+    }
